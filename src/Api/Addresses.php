08c30f92705e6e5d3781650d9dfe25b4b183558c
Hamza Wakrim <wakrimhamza90@gmail.com>
2022-03-18 17:21:27 +0100

    Add Addressess to package

---
+++ src/Api/Addresses.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Grayloon\Magento\Api;
+
+use Illuminate\Http\Client\Response;
+
+class Addresses extends AbstractApi
+{
+
+    /**
+     * @param int $id
+     * @param object $body
+     * @return Response|void
+     */
+    public function edit($id, $body)
+    {
+        return $this->put('/customers/' . $id, $body);
+    }
+
+}
